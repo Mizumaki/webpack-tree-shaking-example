1f600943cf2318383ed652c1a2e9fa5c6ee244ee
soarflat <soarflat777@gmail.com>
2018-07-03 21:43:26 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import {
+  map,
+  differenceWith,
+  divide,
+  drop,
+  dropRight,
+  dropRightWhile,
+} from 'lodash';
+import { Row, Grid as MyGrid } from 'react-bootstrap';
+import { cube } from './math.js';
+
+console.log(cube(5));
---
+++ src/math.js
@@ -0,0 +1,8 @@
+// どこにも利用されていないため、デッドコード
+export function square(x) {
+  return x * x;
+}
+
+export function cube(x) {
+  return x * x * x;
+}
